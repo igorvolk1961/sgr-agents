a7814dfe7287c7d0062363273cddd430f9eacf77
igorvolk <igorvolk1961@gmail.com>
2025-11-17 10:52:19 +0300

    подготовка benchmark

---
+++ benchmark/benchmark.bat
@@ -0,0 +1,6 @@
+   python run_simpleqa_bench.py ^
+       --path_to_simpleqa ./data/simple_qa_test_set.csv ^
+       --output_path ./output/simpleqa_bench_results.xlsx ^
+       --n_samples 100 ^
+       --batch_size 10
+pause
